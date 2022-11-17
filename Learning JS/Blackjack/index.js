b0685077f2be0668e73302fa1109e943a195860e
123f0ur5 <nagazaky96@gmail.com>
2022-11-17 18:29:29 -0300

    score

--- Learning JS/Blackjack/index.js
+++ Learning JS/Blackjack/index.js
@@ -2,11 +2,17 @@ let ai_score = 0
 let ai_cards = ""
 let score = 0
 let cards = ""
+let user_score = 0
+let computer_score = 0
+let tie = 0
 let message_el = document.getElementById('message')
 let cards_el = document.getElementById('cards')
 let score_el = document.getElementById('score')
 let status_el = document.getElementById('status')
 let winlose_el = document.getElementById('winlose')
+let user_score_el = document.getElementById('user-score')
+let computer_score_el = document.getElementById('computer-score')
+let tie_el = document.getElementById('ties')
 let started = false
 
 function start() {
@@ -47,10 +53,16 @@ function stop(){
         message_el.textContent = "Computer score is: " + ai_score
         if (ai_score > 21 || ai_score < score){
             winlose_el.textContent = "You WIN!!"
+            user_score += 1
+            update_score()
         } else if (ai_score == score && score <= 21 && ai_score <= 21) {
             winlose_el.textContent = "It's a TIE :|"
+            tie += 1
+            update_score()
         } else {
             winlose_el.textContent = "You lose :("
+            computer_score += 1
+            update_score()
         }
     }
 }
@@ -60,7 +72,16 @@ function update() {
         stop()
     } else if (score > 21) {
         winlose_el.textContent = "You lose :("
+        computer_score += 1
+        update_score()
     }
     cards_el.textContent = "Cards: " + cards
     score_el.textContent = "Score: " + score
+}
+
+function update_score(){
+    console.log(user_score, computer_score, tie)
+    user_score_el.textContent = "User: " + user_score
+    computer_score_el.textContent = "Computer: " + computer_score
+    tie_el.textContent = "Tie: " + tie
 }
