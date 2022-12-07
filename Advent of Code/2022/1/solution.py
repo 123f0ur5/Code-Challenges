e0c8438b6ddf4194a93f266ee137481bb7082cf9
123f0ur5 <nagazaky96@gmail.com>
2022-12-07 19:34:05 -0300

    fixes

--- Advent of Code/2022/1/solution.py
+++ Advent of Code/2022/1/solution.py
@@ -1,26 +1,25 @@
 #Part 1
-# highest = 0
-# sum = 0
+highest = 0
+soma1 = 0
 
-# with open('input.txt', 'r') as inp: #Check if you're in the right repository on terminal
-#     values = inp.read().split('\n')
+with open('input.txt', 'r') as inp: #Check if you're in the right repository on terminal
+    values = inp.read().split('\n')
 
 
-# for value in values:
-#     if value == '':
-#         if sum > highest:
-#             highest = sum
+for value in values:
+    if value == '':
+        if soma1 > highest:
+            highest = soma1
 
-#         sum = 0
-#     else:
-#         sum += int(value)
+        soma1 = 0
+    else:
+        soma1 += int(value)
 
-# print(highest)
+print(highest)
+
 #Part 2
 top3 = [0,0,0]
 soma = 0
-with open('input.txt', 'r') as inp: #Check if you're in the right repository on terminal
-     values = inp.read().split('\n')
 
 for value in values:
     if value == '':
--- Advent of Code/2022/2/solution.py
+++ Advent of Code/2022/2/solution.py
@@ -1,34 +1,32 @@
 #Part 1
-# total = 0
-# choices = {
-#      'X' : 1,
-#      'Y' : 2,
-#      'Z' : 3
-# }
+total1 = 0
+choices = {
+     'X' : 1,
+     'Y' : 2,
+     'Z' : 3
+}
 
-# oponent = {
-#      'A' : 'X',
-#      'B' : 'Y',
-#      'C' : 'Z'
-# }
-
-# with open('input.txt', 'r') as inp: #Check if you're in the right repository on terminal
-#      values = inp.read().split('\n')
-
-# for value in values:
-#      total += choices[value[2]]
-
-#      if value[2] == oponent[value[0]]:
-#           total += 3
-#      if (value[2] == 'X' and oponent[value[0]] == 'Z') or (value[2] == 'Y' and oponent[value[0]] == 'X') or (value[2] == 'Z' and oponent[value[0]] == 'Y'):
-#           total += 6
-     
-# print(total)
-#Part 2
-total = 0
+oponent = {
+     'A' : 'X',
+     'B' : 'Y',
+     'C' : 'Z'
+}
 
 with open('input.txt', 'r') as inp: #Check if you're in the right repository on terminal
      values = inp.read().split('\n')
+
+for value in values:
+     total1 += choices[value[2]]
+
+     if value[2] == oponent[value[0]]:
+          total1 += 3
+     if (value[2] == 'X' and oponent[value[0]] == 'Z') or (value[2] == 'Y' and oponent[value[0]] == 'X') or (value[2] == 'Z' and oponent[value[0]] == 'Y'):
+          total1 += 6
+     
+print(total1)
+
+#Part 2
+total2 = 0
 
 choice_draw = {
      'A' : 1,
@@ -50,12 +48,12 @@ choice_lose = {
 
 for value in values:
      if value[2] == 'X':
-          total += choice_lose[value[0]]
+          total2 += choice_lose[value[0]]
      elif value[2] == 'Y':
-          total += 3
-          total += choice_draw[value[0]]
+          total2 += 3
+          total2 += choice_draw[value[0]]
      elif value[2] == 'Z':
-          total += 6
-          total += choice_win[value[0]]
+          total2 += 6
+          total2 += choice_win[value[0]]
 
-print(total)
+print(total2)
