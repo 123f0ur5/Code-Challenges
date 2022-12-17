d285e7835883db9b4bf6fc22d325f741b7965114
123f0ur5 <nagazaky96@gmail.com>
2022-12-17 10:24:32 -0300

    6-2

--- Advent of Code/2022/6/solution.py
+++ Advent of Code/2022/6/solution.py
@@ -1,7 +1,9 @@
+#Part 1
 cont = 4
 
 with open('input.txt', 'r') as inp: #Check if you're in the right repository on terminal
      value = inp.read()
+
 
 for i in range(len(value)):
     check = set(value[i:i+4])
@@ -11,3 +13,16 @@ for i in range(len(value)):
         cont += 1
 
 print(cont)
+
+#Part 2
+
+cont2 = 14
+
+for i in range(len(value)):
+    check = set(value[i:i+14])
+    if len(check) == 14:
+        break
+    else:
+        cont2 += 1
+
+print(cont2)
